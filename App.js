3bc4f2576d7c5d388b8695bea1db218bfe26f3ea
dinesh.kumar1 <dinesh.kumar1@inadev.com>
2023-04-15 11:55:52 +0530

    second commit, Chapter-2

--- App.js
+++ App.js
@@ -1,3 +1,29 @@
+/*
+* HMR- Hot Module Replacement
+* File Watcher Algorithm
+* Budling
+* Minify
+* Cleaning our code
+* Dev and Production Build
+* Super Fast build algorithm
+* Image optimization
+* Caching while Development
+* Compression
+* Compatible with older version of browser
+* HTTPS on dev npx parcel index.html --https
+* Port Number
+* Consistent Hashing 
+* Zero Config
+* Browserslist -- https://www.npmjs.com/package/browserslist,  https://browserslist.dev/
+*
+*
+*
+* Transitive Dependencies- we have our pacakge manager which handles and takes care of our transitive dependencies our code 
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 // const heading = React.createElement("h1", {id: "title"}, "Heading 1");
 const heading1 = React.createElement("h1", { id: "title" }, "Heading 1");
 const heading2 = React.createElement("h2", { id: "title" }, "Heading2");
