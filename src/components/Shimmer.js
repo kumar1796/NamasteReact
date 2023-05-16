172dcf1b8ccf9409f95f4c02c8db7d6f63a17702
dinesh.kumar1 <dinesh.kumar1@inadev.com>
2023-05-16 22:29:46 +0530

    seventh commit, chapter 7, previous commit has an error, in this commit, restaurant card is not showing data as swiggy API is restructured

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -6,7 +6,7 @@ const Shimmer = () => {
     <div className="restaurant-list">
       {Array(10)
         .fill("")
-        .map((e) => (
+        .map((e,index) => (
           <div key={index} className='shimmer-card' ></div>
         ))}
     </div>
